c1b91fefb96a5399e69d340de2448165b9bf64a1
maikelekiam <miarcamone@gmail.com>
2017-05-19 09:50:44 -0300

    commit inicial

---
+++ Sistema-CyT/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sistema_CyT.Startup))]
+namespace Sistema_CyT
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
