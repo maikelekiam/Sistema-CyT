74b4478f72b3452543be8b2074652b2b13e6b201
maikelekiam <miarcamone@gmail.com>
2017-05-22 11:26:50 -0300

    Falla CapaRepositorio

---
+++ CapaRepositorio/FondoRepo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDominio;
+
+namespace CapaRepositorio
+{
+    public class FondoRepo
+    {
+        public void GuardarFondo(Fondo fondo)
+        {
+            using (ModeloDeDominio modeloDeDominio = new ModeloDeDominio())
+            {
+                modeloDeDominio.Add(fondo);
+                modeloDeDominio.SaveChanges();
+            }
+        }
+
+        public IEnumerable<Fondo> MostrarFondos()
+        {
+            using (ModeloDeDominio modeloDeDominio = new ModeloDeDominio())
+            {
+                IEnumerable<Fondo> result = modeloDeDominio.Fondos.ToList();
+                return result;
+            }
+        }
+
+        public Fondo ObtenerFondo(int id)
+        {
+            using (ModeloDeDominio modeloDeDominio = new ModeloDeDominio())
+            {
+                Fondo fondo = modeloDeDominio.Fondos.Where(c => c.IdFondo == id).FirstOrDefault();
+
+                return fondo;
+            }
+        }
+
+        public void ActualizarFondo(Fondo fondo)
+        {
+            using (ModeloDeDominio modeloDeDominio = new ModeloDeDominio())
+            {
+                modeloDeDominio.AttachCopy(fondo);
+                modeloDeDominio.SaveChanges();
+            }
+        }
+
+        public void EliminarFondo(int id)
+        {
+            using (ModeloDeDominio modeloDeDominio = new ModeloDeDominio())
+            {
+                IQueryable<Fondo> query = modeloDeDominio.GetAll<Fondo>().Where(c => c.IdFondo == id);
+
+                foreach (Fondo fondo in query)
+                {
+                    modeloDeDominio.Delete(fondo);
+                    modeloDeDominio.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ CapaRepositorio/OrigenRepo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDominio;
+
+
+namespace CapaRepositorio
+{
+    public class OrigenRepo
+    {
+        public void GuardarOrigen(Origen origen)
+        {
+            using (ModeloDeDominio modeloDeDominio = new ModeloDeDominio())
+            {
+                modeloDeDominio.Add(origen);
+                modeloDeDominio.SaveChanges();
+            }
+        }
+
+        public IEnumerable<Origen> MostrarOrigenes()
+        {
+            using (ModeloDeDominio modeloDeDominio = new ModeloDeDominio())
+            {
+                IEnumerable<Origen> result = modeloDeDominio.Origens.ToList();
+                return modeloDeDominio.CreateDetachedCopy(result);
+            }
+        }
+
+        public string TraerOrigenSegunIdFondo(int id)
+        {
+            using (ModeloDeDominio modeloDeDominio = new ModeloDeDominio())
+            {
+                Origen result = modeloDeDominio.Origens.Where(c => c.IdOrigen == id).FirstOrDefault();
+
+                modeloDeDominio.CreateDetachedCopy(result);
+                return result.Nombre;
+            }
+        }
+
+        public int TraerOrigenIdSegunItem(string item)
+        {
+            using (ModeloDeDominio modeloDeDominio = new ModeloDeDominio())
+            {
+                Origen result = modeloDeDominio.Origens.Where(c => c.Nombre == item).FirstOrDefault();
+
+                modeloDeDominio.CreateDetachedCopy(result);
+
+                return result.IdOrigen;
+            }
+        }
+    }
+}
